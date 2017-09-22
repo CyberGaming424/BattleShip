36548fcc1713c4b601d47210efc2223890656857
CyberGaming424 <jlake624@gmail.com>
2017-09-21 23:04:56 -0400

    A few changes on how variables work.

--- BattleShip_NonGUI.py
+++ BattleShip_NonGUI.py
@@ -28,61 +28,58 @@ class Create_player:
 
 
 # Creates the class to start actually play the game
+def attack():
+    # Prints how to attack
+    print(
+        "This is the attack phase of the game. To attack you will be give three rows to place one shot in. A 1 is "
+        "considered a shot")
+    # Askes where you want to attack in the first row
+    global attack_row1
+    attack_row1 = input("Row one:")
+    # Askes where you want to attack in the second row
+    global row2
+    attack_row2 = input("Row two:")
+    # Askes where you want to attack in the second row
+    global row3
+    attack_row3 = input("Row three:")
+
+
 class Game:
     # Set the number of player
     def players(self):
-        # Creates the global variable amount_of_players
-        global amount_of_players
+
         # Asks how many players are gonna play and set amount_of_players to the number they input
-        amount_of_players = input("How many players do you have?")
+        self.amount_of_players = input("How many players do you have?")
 
     # Creates the objects that each player needs to play the game
     def if_players(self):
         # Calls the player function so you can use the vaiable amount_of_players
-        Game.players()
         # Checks to see if the amount of players is 1
-        if amount_of_players == "1":
+        if self.amount_of_players == "1":
             # If so print that you need friends to play this game
             print("You need friends to play this game.")
         # Checks to see if the amount of players is 2
-        elif amount_of_players == "2":
+        elif self.amount_of_players == "2":
             # Creates objects for the players positions and maps
-            global player1_position
-            player1_position = Create_player.choose_player()
-            global player1_map
-            player1_map = Create_player.set_Map()
-            global player2_position
-            player2_position = Create_player.choose_player()
-            global player2_map
-            player2_map = Create_player.set_Map()
+            def player1_position(self):
+                self.player1_position = Create_player.choose_player()
+            self.player1_map = Create_player.set_Map()
+            self.player2_position = Create_player.choose_player()
+            self.player2_map = Create_player.set_Map()
+
 
     # Creates the function used to attack other players
-    @staticmethod
-    def attack():
-        # Prints how to attack
-        print(
-            "This is the attack phase of the game. To attack you will be give three rows to place one shot in. A 1 is "
-            "considered a shot")
-        # Askes where you want to attack in the first row
-        global attack_row1
-        attack_row1 = input("Row one:")
-        # Askes where you want to attack in the second row
-        global row2
-        attack_row2 = input("Row two:")
-        # Askes where you want to attack in the second row
-        global row3
-        attack_row3 = input("Row three:")
 
-    def hit_or_miss():
-        Game.attack()
-        Game.if_players()
-        while amount_of_players == "2":
-            if attack.attack_row1 == if_players.player1_map.row1:
+    def hit_or_miss(self):
+        while self.amount_of_players == "2":
+            if self.attack_row1 == self.if_players.player1_map.row1:
                 print("Hit")
 
 
-class main():
-    Game.if_players()
+
+class Main():
+    game1 = Game()
+    Game.players()
 
 
-main()
+Main()
